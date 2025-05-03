4c31358a1341b89efee651702a424c9419b34027
Pooja675 <poojasinha675@gmail.com>
2025-05-03 15:59:28 +0530

    Setting up project and MongoDB

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,9 @@
+import {StreamChat} from "stream-chat";
+import "dotenv/config"
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API key of Secret is missing")
+}
