f8daf4de2da0d7083d2cd0f5a5c28e8bfc6c1a12
Pooja675 <poojasinha675@gmail.com>
2025-05-03 16:34:41 +0530

    Completed login and logout

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -7,3 +7,19 @@ const apiSecret = process.env.STREAM_API_SECRET;
 if(!apiKey || !apiSecret){
     console.error("Stream API key of Secret is missing")
 }
+
+const streamChat = StreamChat.getInstance(apiKey, apiSecret)
+
+export const upsertStreamUser = async (userData) => {
+
+    try {
+        await streamChat.upsertUsers([userData])
+        return userData;
+    } catch (error) {
+        console.error("Error upserting stream user", error)
+    }
+}
+
+//todo : do it later
+
+//export const generateStreamToken  = (userId) = {}
