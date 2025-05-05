35ff89b985111c0c5d0054e80bd36bf568c40025
Pooja675 <poojasinha675@gmail.com>
2025-05-05 10:03:54 +0530

    Accept friend request api

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
  const router = express.Router();
 
@@ -11,5 +11,7 @@ import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../control
  router.get("/friends", getMyFriends)
 
  router.post("/friend-request/:id", sendFriendRequest)
+ router.post("/friend-request/:id/accept", acceptFriendRequest)
+
 
  export default router;
