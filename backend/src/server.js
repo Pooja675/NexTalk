69561dd7680e751c25c258c99000a7a12b895ae6
Pooja675 <poojasinha675@gmail.com>
2025-05-12 12:33:35 +0530

    Updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,15 @@ import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
+
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
+
+const _dirname = path.resolve()
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -24,6 +28,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
 
+if(process.env.NODE_ENV =="production"){
+    app.use(express.static(path.join(_dirname, "../frontend/dist")))
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname,"../frontend", "dist", "index.html"))
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
  export const useThemeStore = create((set) => ({
- theme: localStorage.getItem("nextalk-theme") || "coffee",
+ theme: localStorage.getItem("nextalk-theme") || "valentine",
  setTheme: (theme) =>{
     localStorage.setItem("nextalk-theme", theme);
     set({theme});
